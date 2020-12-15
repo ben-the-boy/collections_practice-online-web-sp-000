f5ca9e416a42edb12b50995efc2c032046085fef
Ben Hayes <b.t.hayes@comcast.net>
2020-12-15 19:47:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array [2] = array[2], array[1]
+  array
+end 
+
+def reverse_array(array)
+  new_array = array.reverse
+  new_array
+end
+  
+def kesha_maker(array)
+  new_array = []
+  array.each do |word|
+    new_array << 
+end
+
+
+
+
+
+
+
+
+  
+  
+  
+  
