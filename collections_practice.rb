053f6b037e92640ea9a3e0e3e994c20b8d876715
Ben Hayes <b.t.hayes@comcast.net>
2020-12-15 20:22:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,9 +47,7 @@ end
 
 def add_s(array)
   array.each_with_index.collect do |element, index|
-    if index == 1 
-      return element 
-    else
+    if index != 1 
       element << "s"
     end
   end
